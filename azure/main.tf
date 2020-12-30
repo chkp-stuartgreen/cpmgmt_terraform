da962578a891327a4390424819a97c3c87ba3cd9
Stuart Green <stuartg@checkpoint.com>
2020-12-30 15:32:24 +0000

    More tidying, removed hard coded region

--- azure/main.tf
+++ azure/main.tf
@@ -1,7 +1,5 @@
 # Basic resources
 
-
-
 resource "azurerm_resource_group" "rg_mgmt" {
   name = var.rg_name
   location = var.az_location
@@ -11,20 +9,25 @@ resource "azurerm_linux_virtual_machine" "vm_logserver" {
   name = var.log_server_name
   resource_group_name = var.rg_name
   location = var.az_location
+  # Feel free to change the VM size to something more suitable if required
   size = "Standard_D3_v2"
   admin_username = var.admin_username
   admin_password = var.admin_password
-  # WIP
+  
+  # If a public key is provided, we will disabled interactive authentication
   disable_password_authentication = var.admin_public_key_file_path != "" ? true : false
-  #disable_password_authentication = false
+  
   network_interface_ids = [ azurerm_network_interface.mgmt_nic.id ]
-  custom_data = base64encode("#!/usr/bin/python3 /etc/cloud_config.py\n\ninstallationType=\"management\"\nallowUploadDownload=\"true\"\nosVersion=\"r8040\"\ntemplateName=\"management\"\nisBlink=\"false\"\ntemplateVersion=\"20201109\"\nbootstrapScript64=\"\"\nlocation=\"West Europe\"\nmanagementGUIClientNetwork=\"0.0.0.0/0\"\n")
+  custom_data = base64encode("#!/usr/bin/python3 /etc/cloud_config.py\n\ninstallationType=\"management\"\nallowUploadDownload=\"true\"\nosVersion=\"r8040\"\ntemplateName=\"management\"\nisBlink=\"false\"\ntemplateVersion=\"20201109\"\nbootstrapScript64=\"\"\nlocation=\"${var.az_location}\"\nmanagementGUIClientNetwork=\"0.0.0.0/0\"\n")
   source_image_reference {
     publisher = "checkpoint"
     offer = "check-point-cg-r8040"
     sku = "mgmt-byol"
     version = "latest"
   }
+
+  # This section will only be added if you have specified a path to your
+  # public key file in the terraform.tfvars file
   dynamic "admin_ssh_key" {
     for_each = var.admin_public_key_file_path != "" ? [1] : []
     content {
@@ -32,11 +35,6 @@ resource "azurerm_linux_virtual_machine" "vm_logserver" {
       public_key = file(var.admin_public_key_file_path)
     }
   }
-  
-  #admin_ssh_key {
-  #  username = "admin"
-  #  public_key = file(var.admin_public_key_path)
-  #  }  
 
   plan {
     name = "mgmt-byol"
